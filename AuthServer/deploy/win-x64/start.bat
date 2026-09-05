a9296d1c2dc9e1e974c7ef069338ba54a1243115
sours3s <141668749+sours3s@users.noreply.github.com>
2026-09-05 16:16:46 +0300

    Delete AuthServer directory

--- AuthServer/deploy/win-x64/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title FluxVisuals AuthServer
-echo ==========================================
-echo  FluxVisuals AuthServer (Windows)
-echo  http://0.0.0.0:5001
-echo ==========================================
-cd /d %~dp0
-AuthServer.exe
-pause
