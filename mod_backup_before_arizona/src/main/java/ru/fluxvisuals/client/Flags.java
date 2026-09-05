e33a7475d8bd0ea3543943aa8e986e7f640dc8cb
sours3s <141668749+sours3s@users.noreply.github.com>
2026-09-05 16:17:28 +0300

    Delete mod_backup_before_arizona directory

--- mod_backup_before_arizona/src/main/java/ru/fluxvisuals/client/Flags.java
+++
@@ -1,7 +0,0 @@
-package ru.fluxvisuals.client;
-
-/** Глобальные флаги для коммуникации между модулями и миксинами (без циклических зависимостей). */
-public final class Flags {
-    public static boolean fireOverlayDisabled = false;
-    private Flags() {}
-}
